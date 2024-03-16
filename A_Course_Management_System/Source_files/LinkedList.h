89fbf9b7831be970a71262da8cee3630283afdfd
khoaphamm <116829205+khoaphamm@users.noreply.github.com>
2024-03-16 16:30:03 +0700

    LinkedList implementation

--- A_Course_Management_System/Source_files/LinkedList.h
+++ A_Course_Management_System/Source_files/LinkedList.h
@@ -1,5 +1,7 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
+#include "Student.h"
+#include <stdexcept>
 
 template <typename T>
 struct Node {
@@ -10,20 +12,97 @@ struct Node {
         : data(data), next(next) {}
 };
 
+
 template <typename T>
 class LinkedList {
     Node<T>* head;
+    Node<T>* tail;
+    int count;
 
 public:
-    LinkedList() : head(nullptr) {}
+    LinkedList() : head(nullptr), tail(nullptr), count(0) {}
 
-    void add(const T& data);
+    void add(const T& data){
+         Node<T>* newNode = new Node<T>(data);
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
 
-    void remove(const T& data);
+    void addSorted(const T& data) {
+        Node<T>* newNode = new Node<T>(data);
+        if (head == nullptr || *data <= *head->data) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node<T>* current = head;
+            while (current->next != nullptr && *current->next->data < *data) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+        count++;
+   }
 
-    Node<T>* find(const T& data) const;
+    void remove(const T& data){
+        Node<T>* current = head;
+        Node<T>* prev = nullptr;
+        while (current != nullptr && current->data != data) {
+            prev = current;
+            current = current->next;
+        }
+        if (current == nullptr) return; // data not found
+        if (prev == nullptr) { // data is in the first node
+            head = current->next;
+            if (head == nullptr) tail = nullptr; // list is now empty
+        } else {
+            prev->next = current->next;
+            if (current->next == nullptr) tail = prev; // data is in the last node
+        }
+        delete current;
+        count--;
+    }
+
+    Node<T>* find(const T& data) const{
+        Node<T>* current = head;
+        while (current != nullptr && current->data != data) {
+            current = current->next;
+        }
+        return current; // returns nullptr if data not found
+    }
     
-    size_t size() const;
+    int size() const{
+        return count;
+    }
+
+    T& get(int index) {
+        if (index < 0 || index >= count) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        Node<T>* current = head;
+        for (int i = 0; i < index; i++) {
+            current = current->next;
+        }
+
+        return current->data;
+    }
+
+    // ~LinkedList(){
+    //     Node<T>* current = head;
+    //     while (current != nullptr) {
+    //         Node<T>* next = current->next;
+    //         delete current;
+    //         current = next;
+    //     }
+    // }
 };
+
+
 
 #endif // LINKEDLIST_H
