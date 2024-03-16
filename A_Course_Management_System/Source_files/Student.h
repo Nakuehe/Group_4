4f453e893d5db5d62b8b0d377fed16f63fbf8d4b
khoaphamm <116829205+khoaphamm@users.noreply.github.com>
2024-03-16 16:30:16 +0700

    Update header files

--- A_Course_Management_System/Source_files/Student.h
+++ A_Course_Management_System/Source_files/Student.h
@@ -1,26 +1,40 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include "User.h"
-#include "Student.h"
 #include <string>
-#include <vector>
 
-struct Student : public User {
+
+struct Student{
     std::string studentID;
     std::string firstName;
     std::string lastName;
     std::string gender;
     std::string dateOfBirth;
     std::string socialID;
-    // LinkedList<Course> courses;
 
-    Student(const std::string& username, const std::string& password, const std::string& studentID, const std::string& firstName, const std::string& lastName, const std::string& gender, const std::string& dateOfBirth, const std::string& socialID)
-        : User(username, password), studentID(studentID), firstName(firstName), lastName(lastName), gender(gender), dateOfBirth(dateOfBirth), socialID(socialID) {}
+    Student(const std::string& studentID, const std::string& firstName, const std::string& lastName, const std::string& gender, const std::string& dateOfBirth, const std::string& socialID)
+        :studentID(studentID), firstName(firstName), lastName(lastName), gender(gender), dateOfBirth(dateOfBirth), socialID(socialID) {}
+
 
     void viewCourses();
 
     void viewScoreboard();
+
+    bool operator==(const Student& other) const {
+        return studentID == other.studentID;
+    }
+
+    bool operator!=(const Student& other) const {
+        return !(*this == other);
+    }
+
+    bool operator>(const Student& other) const {
+        return studentID > other.studentID;
+    }
+
+    bool operator<(const Student& other) const {
+        return studentID < other.studentID;
+    }
 };
 
 #endif // STUDENT_H
