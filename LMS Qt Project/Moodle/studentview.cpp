c4b79ec99f9439c8930d813d587bd8b98c98a339
khoaphamm <116829205+khoaphamm@users.noreply.github.com>
2024-04-03 16:40:08 +0700

    Table grade view(student finished)

--- LMS Qt Project/Moodle/studentview.cpp
+++ LMS Qt Project/Moodle/studentview.cpp
@@ -4,7 +4,14 @@
 #include "courseinfowindow.h"
 #include <QMessageBox>
 #include <QCloseEvent>
-
+#include <QScrollBar>
+#include <QTimer>
+#include <QDebug>
+#include <QTableView>
+#include <QStandardItemModel>
+#include <QHeaderView>
+#include <QPainterPath>
+#include <QFont>
 
 
 
@@ -80,6 +87,7 @@ StudentView::~StudentView()
     delete ui;
     delete thisStudentCourse;
     delete thisStudentScore;
+    delete model;
 }
 
 
@@ -113,6 +121,8 @@ void StudentView::setupPage(){
 
     //hide full menu widget
     ui->full_menu_widget->hide();
+    connect(ui->menu_btn, &QPushButton::toggled, this, &StudentView::menu_btn_toggled);
+
 
     //initiallize page
     ui->stackedWidget->setCurrentIndex(1);
@@ -127,7 +137,7 @@ void StudentView::setupPage(){
     ui->changePasswordButton->setCursor(QCursor(Qt::PointingHandCursor));
 
     //round avatar
-    
+
     RoundAvatarLabel *avatar = new RoundAvatarLabel(ui->avatar);
     avatar->setGeometry(0, 0, ui->avatar->width(), ui->avatar->height()); // Set the geometry to match the QLabel
 }
@@ -167,10 +177,10 @@ void StudentView::setUpCourseList() {
         btn_course->setStyleSheet("QPushButton:hover { color: #ff6600; } QPushButton:pressed { background-color: transparent; } QPushButton { text-align: left; font-weight: 500; color: #0D63E6; border: none; }");
         btn_course->setFont(QFont(fontFamily2, 16));
         btn_course->setCursor(QCursor(Qt::PointingHandCursor));
-        
+
         btn_course->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Preferred);
 
-        
+
         label->setTextFormat(Qt::RichText);
         label->setStyleSheet("QWidget { border: none; }");
         widgetLayout->addWidget(btn_course);
@@ -198,7 +208,7 @@ void StudentView::setUpCourseList() {
                 CourseInfoWindow *window = new CourseInfoWindow(this, &curCourse, i);
                 window->show();
             });
-            
+
             std::string time;
             std::string session = curCourse.session;
             if (session == "S1") {
@@ -233,8 +243,12 @@ void StudentView::setUpCourseList() {
 void StudentView::setUpGradeView(){
 
     //TODO: set up grade view design
-    
-    QStandardItemModel *model = new QStandardItemModel(thisStudentScore->size(), 6, this);
+
+    QString fontFamilyRegular = loadFont(":/asset/font/HelveticaWorld-Regular.ttf");
+    QString fontFamilyMedium = loadFont(":/asset/font/Helvetica Neue/helveticaneuemedium.ttf");
+    QString fontFamilyBold = loadFont(":/asset/font/Helvetica Neue/HelveticaNeue-Bold.otf");
+
+    model = new QStandardItemModel(thisStudentScore->size(), 6, this);
     for (int row = 0; row < thisStudentScore->size(); ++row) {
         QString courseID = QString::fromStdString(thisStudentScore->get(row).courseID);
         QString courseName = QString::fromStdString(thisStudentScore->get(row).courseName);
@@ -245,27 +259,115 @@ void StudentView::setUpGradeView(){
 
         QStandardItem *item1 = new QStandardItem(QString(courseID));
         model->setItem(row, 0, item1);
+
         QStandardItem *item2 = new QStandardItem(QString(courseName));
         model->setItem(row, 1, item2);
+
         QStandardItem *item3 = new QStandardItem(QString(midterm));
+        item3->setTextAlignment(Qt::AlignCenter);
         model->setItem(row, 2, item3);
+
         QStandardItem *item4 = new QStandardItem(QString(final));
+        item4->setTextAlignment(Qt::AlignCenter);
         model->setItem(row, 3, item4);
+
         QStandardItem *item5 = new QStandardItem(QString(bonus));
+        item5->setTextAlignment(Qt::AlignCenter);
         model->setItem(row, 4, item5);
+
         QStandardItem *item6 = new QStandardItem(QString(total));
+        item6->setTextAlignment(Qt::AlignCenter);
         model->setItem(row, 5, item6);
     }
 
-    QTableView *tableView = new QTableView;
-    tableView->setModel(model);
-    tableView->setEditTriggers(QAbstractItemView::NoEditTriggers); // Make the table view uneditable
+    // // Set the stylesheet
+    // ui->grade_view_table->setStyleSheet("QTableView {"
+    //                                     "background-color: #E8E8E8;" // Set the background color
+    //                                     "gridline-color: #606060;" // Set the gridline color
+    //                                     "}"
+    //                                     "QTableView::item {"
+    //                                     "border: 1px solid #606060;" // Set the border of the items
+    //                                     "}"
+    //                                     "QTableView::item:selected {"
+    //                                     "background-color: #909090;" // Set the background color of the selected items
+    //                                     "}");
 
-    QWidget *page = ui->stackedWidget->widget(2); // Get the page at index 2
+    model->setHeaderData(0, Qt::Horizontal, tr("Course ID"));
+    model->setHeaderData(1, Qt::Horizontal, tr("Course Name"));
+    model->setHeaderData(2, Qt::Horizontal, tr("Midterm"));
+    model->setHeaderData(3, Qt::Horizontal, tr("Final"));
+    model->setHeaderData(4, Qt::Horizontal, tr("Bonus"));
+    model->setHeaderData(5, Qt::Horizontal, tr("Total"));
 
-    QVBoxLayout *layout = new QVBoxLayout(page); // Create a QVBoxLayout for the page
-    layout->addWidget(tableView); // Add the QTableView to the layout
-    page->setLayout(layout); // Set the layout of the page
+    ui->grade_view_table->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
+    ui->grade_view_table->horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
+
+    ui->grade_view_table->setDragEnabled(false);
+    ui->grade_view_table->setDragDropOverwriteMode(false);
+    ui->grade_view_table->setDragDropMode(QAbstractItemView::NoDragDrop);
+    ui->grade_view_table->setSelectionMode(QAbstractItemView::NoSelection);
+
+    // QRect rect = ui->grade_view_table->viewport()->rect();
+    // QPainterPath path;
+    // // Adjust the rect to ensure the mask covers the entire viewport area
+    // rect.adjust(+1, +1, -1, -1);
+    // path.addRoundedRect(rect, 10, 10);
+    // QRegion mask = QRegion(path.toFillPolygon().toPolygon());
+    // ui->grade_view_table->viewport()->setMask(mask);
+
+
+    // Set the model
+    ui->grade_view_table->setModel(model);
+
+    // Make the table view uneditable
+    ui->grade_view_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    // Delay the resizing of the columns
+    QTimer::singleShot(0, this, SLOT(resizeColumns()));
+
+    // Set the font of the table
+    //ui->grade_view_table->horizontalHeader()->setStyleSheet("QHeaderView { font-size: 12pt; font-weight: bold;}");
+    ui->grade_view_table->setFont(QFont(fontFamilyRegular, 12));
+
+
+
+    // Set the font of the header
+
+}
+
+void StudentView::resizeColumns() {
+    ui->grade_view_table->resizeColumnsToContents();
+
+    bool isScrollBarVisible = ui->grade_view_table->verticalScrollBar()->isVisible();
+
+    // Calculate the total content width
+    int totalContentWidth = 0;
+    for (int i = 0; i < model->columnCount(); ++i) {
+        totalContentWidth += ui->grade_view_table->columnWidth(i);
+    }
+
+    //Calculate total width without tab
+    int totalAvailableWidth = ui->stackedWidget->width();
+    // qDebug() << "1. " << totalAvailableWidth;
+    // qDebug() << "2. " << ui->grade_view_table->viewport()->width();
+
+    int usedWidth = 0;
+
+    // Set the width of each column
+    int i;
+
+    for (i = 0; i < model->columnCount() - 1; ++i) {
+        int stretchFactor = ui->grade_view_table->columnWidth(i) * 100 / totalContentWidth;
+        int newWidth = stretchFactor * totalAvailableWidth / 100;
+        ui->grade_view_table->setColumnWidth(i, newWidth);
+        usedWidth += newWidth;
+    }
+
+    if(!isScrollBarVisible)
+        ui->grade_view_table->setColumnWidth(i, totalAvailableWidth - usedWidth - 35); // Set the width of the last column
+    else
+        ui->grade_view_table->setColumnWidth(i, totalAvailableWidth - usedWidth - 35 - ui->grade_view_table->verticalScrollBar()->width()); // Set the width of the last column
+
 }
 
 void StudentView::setUpProfile(){
@@ -359,6 +461,11 @@ void StudentView::setUpProfile(){
 
 }
 
+void StudentView::resizeEvent(QResizeEvent *event) {
+    QMainWindow::resizeEvent(event);  // Call the base class implementation
+    resizeColumns();  // Resize the columns
+}
+
 void StudentView::on_profile_btn1_toggled() {
     ui->stackedWidget->setCurrentIndex(0);
 }
@@ -386,4 +493,13 @@ void StudentView::on_grade_btn2_toggled() {
 void StudentView::on_changePasswordButton_clicked() {
     ChangePasswordDialog dialog(this, thisStudentUser, s_UserManager);
     dialog.exec();
+}
+
+void StudentView::menu_btn_toggled(bool checked) {
+    // Show or hide the full_menu_widget and icon_only_widget
+    ui->icon_only_widget->setHidden(checked);
+    ui->full_menu_widget->setVisible(checked);
+
+    // Schedule resizeColumns to be called after the layout has been updated
+    QTimer::singleShot(0, this, &StudentView::resizeColumns);
 }
