fae48c88d550e11e1b499e20e3f27ff9cf900740
Tram <tram14042005@gmail.com>
2024-04-07 21:06:50 +0700

    Create Semester in QT

--- LMS Qt Project/Moodle/header_files/SchoolYear.h
+++ LMS Qt Project/Moodle/header_files/SchoolYear.h
@@ -1,6 +1,6 @@
 #include "LinkedList.h"
 #include "Semester.h"
-
+#include <QInputDialog>
 struct SchoolYear {
     std::string year;
     std::string start_date; // format: dd/mm/yyyy
@@ -12,7 +12,35 @@ struct SchoolYear {
 
 
     void createYear();
-    void createSemester();
+    void createSemester()
+    {
+        if(this->semesters.size()>2) return;
+        else
+        {
+        QString start_date = QInputDialog::getText(nullptr, "Input start date", "Enter start date:");
+        std::string start_date_std = start_date.toStdString();
+        QString end_date = QInputDialog::getText(nullptr, "Input end date", "Enter end date:");
+        std::string end_date_std = end_date.toStdString();
+        if(this->semesters.size()==0) {
+            Semester new_semester("Semester 1");
+            new_semester.start_date = start_date_std;
+            new_semester.end_date = end_date_std;
+            this->addSemester(new_semester);
+        }
+        else if(this->semesters.size()==1) {
+            Semester new_semester("Semester 2");
+            new_semester.start_date = start_date_std;
+            new_semester.end_date = end_date_std;
+            this->addSemester(new_semester);
+        }
+        else if(this->semesters.size()==2) {
+            Semester new_semester("Semester 3");
+            new_semester.start_date = start_date_std;
+            new_semester.end_date = end_date_std;
+            this->addSemester(new_semester);
+        }
+        }
+    }
     void addSemester(const Semester& semester) {
         semesters.add(semester);
     }
